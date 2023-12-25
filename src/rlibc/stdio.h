e16392f923976413d7490826074af309ebed0d9e
SetScript <101998415+SetScript@users.noreply.github.com>
2023-12-25 15:38:03 +0300

    move idt to src/kernel/idt.c

--- src/rlibc/stdio.h
+++ src/rlibc/stdio.h
@@ -1,19 +1,11 @@
-#define LINES 25
-#define COLUMNS_IN_LINE 80
-#define BYTES_FOR_EACH_ELEMENT 2
-#define SCREENSIZE BYTES_FOR_EACH_ELEMENT * COLUMNS_IN_LINE * LINES
-
-#define IDT_SIZE 256
-#define INTERRUPT_GATE 0x8e
-#define KERNEL_CODE_SEGMENT_OFFSET 0x08
-
-#define ENTER_KEY_CODE 0x1C
 unsigned int current_loc = 0;
 int cooldown = 0; /* Note:
 			cooldown is the frequency with which the screen will be
 			cleared. at the moment this function does not work.
 			 	> view 37-50 line
 			*/
+
+
 char *vidptr = (char*)0xb8000;
 
 void print(const char *str) {
@@ -60,3 +52,4 @@ void println(const char *str) {
 	print(str);
 	newline();
 }
+
