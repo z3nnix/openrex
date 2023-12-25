41fa8e89825d20a8bd75997db0c08a135b4af948
SetScript <101998415+SetScript@users.noreply.github.com>
2023-12-25 07:39:57 +0300

    add note

--- src/rlibc/stdio.h
+++ src/rlibc/stdio.h
@@ -9,7 +9,11 @@
 
 #define ENTER_KEY_CODE 0x1C
 unsigned int current_loc = 0;
-
+int cooldown = 0; /* Note:
+			cooldown is the frequency with which the screen will be
+			cleared. at the moment this function does not work.
+			 	> view 37-50 line
+			*/
 char *vidptr = (char*)0xb8000;
 
 void print(const char *str) {
@@ -20,17 +24,36 @@ void print(const char *str) {
 	}
 }
 
-void newline(void) {
-	unsigned int line_size = BYTES_FOR_EACH_ELEMENT * COLUMNS_IN_LINE;
-	current_loc = current_loc + (line_size - current_loc % (line_size));
-}
-
 void clearscreen(void) {
 	unsigned int i = 0;
 	while (i < SCREENSIZE) {
 		vidptr[i++] = ' ';
 		vidptr[i++] = 0x07;
 	}
+	//cooldown == 0; | 12 line
+}
+
+/*
+void newline(void) {
+	if(cooldown = LINES) {
+		clearscreen();
+		unsigned int line_size = BYTES_FOR_EACH_ELEMENT * COLUMNS_IN_LINE;
+		current_loc = current_loc + (line_size - current_loc % (line_size));
+		
+		cooldown == 0; //12 line
+		cooldown++; // 12 line
+	} else {
+		unsigned int line_size = BYTES_FOR_EACH_ELEMENT * COLUMNS_IN_LINE;
+		current_loc = current_loc + (line_size - current_loc % (line_size));
+		cooldown++; // 12 line	
+	}
+}
+	// view 12 line
+*/
+
+void newline(void) {
+	unsigned int line_size = BYTES_FOR_EACH_ELEMENT * COLUMNS_IN_LINE;
+	current_loc = current_loc + (line_size - current_loc % (line_size));
 }
 
 void println(const char *str) {
