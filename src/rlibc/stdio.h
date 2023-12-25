873d30c79ecd5a4abaffd7a8869dc3ed9c8f971b
SetScript <101998415+SetScript@users.noreply.github.com>
2023-12-25 17:47:15 +0300

    add scroll

--- src/rlibc/stdio.h
+++ src/rlibc/stdio.h
@@ -1,20 +1,6 @@
 unsigned int current_loc = 0;
-int cooldown = 0; /* Note:
-			cooldown is the frequency with which the screen will be
-			cleared. at the moment this function does not work.
-			 	> view 37-50 line
-			*/
-
 
 char *vidptr = (char*)0xb8000;
-
-void print(const char *str) {
-	unsigned int i = 0;
-	while (str[i] != '\0') {
-		vidptr[current_loc++] = str[i++];
-		vidptr[current_loc++] = 0x07;
-	}
-}
 
 void clearscreen(void) {
 	unsigned int i = 0;
@@ -22,26 +8,28 @@ void clearscreen(void) {
 		vidptr[i++] = ' ';
 		vidptr[i++] = 0x07;
 	}
-	//cooldown == 0; | 12 line
 }
 
-/*
-void newline(void) {
-	if(cooldown = LINES) {
-		clearscreen();
-		unsigned int line_size = BYTES_FOR_EACH_ELEMENT * COLUMNS_IN_LINE;
-		current_loc = current_loc + (line_size - current_loc % (line_size));
-		
-		cooldown == 0; //12 line
-		cooldown++; // 12 line
-	} else {
-		unsigned int line_size = BYTES_FOR_EACH_ELEMENT * COLUMNS_IN_LINE;
-		current_loc = current_loc + (line_size - current_loc % (line_size));
-		cooldown++; // 12 line	
+void scroll_up(void) {
+	clearscreen();
+    unsigned int i;
+    for (i = current_loc; i < SCREENSIZE; i++) {
+        vidptr[i - current_loc] = vidptr[i];
+    }
+    current_loc = 0;
+}
+
+void print(const char *str) {
+	unsigned int i = 0;
+	while (str[i] != '\0') {
+		vidptr[current_loc++] = str[i++];
+		vidptr[current_loc++] = 0x07;
+	}
+	
+	if (current_loc >= SCREENSIZE) {
+        	scroll_up();
 	}
 }
-	// view 12 line
-*/
 
 void newline(void) {
 	unsigned int line_size = BYTES_FOR_EACH_ELEMENT * COLUMNS_IN_LINE;
@@ -52,4 +40,3 @@ void println(const char *str) {
 	print(str);
 	newline();
 }
-
