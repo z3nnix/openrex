d7a7237cd16dca5de3d8b63607b202a90d07525a
SetScript <101998415+SetScript@users.noreply.github.com>
2023-12-25 03:11:38 +0300

    add files

---
+++ src/kernel/kernel.c
@@ -0,0 +1,10 @@
+#include "../rlibc/stdio.h"
+
+void kmain(void) {
+	clearscreen();
+	print(":: boot..");
+	println();
+	print(":: kernel is start.");
+
+	return;
+}
---
+++ src/rlibc/stdio.h
@@ -0,0 +1,37 @@
+#define LINES 25
+#define COLUMNS_IN_LINE 80
+#define BYTES_FOR_EACH_ELEMENT 2
+#define SCREENSIZE BYTES_FOR_EACH_ELEMENT * COLUMNS_IN_LINE * LINES
+
+#define IDT_SIZE 256
+#define INTERRUPT_GATE 0x8e
+#define KERNEL_CODE_SEGMENT_OFFSET 0x08
+
+#define ENTER_KEY_CODE 0x1C
+unsigned int current_loc = 0;
+
+char *vidptr = (char*)0xb8000;
+
+void print(const char *str)
+{
+	unsigned int i = 0;
+	while (str[i] != '\0') {
+		vidptr[current_loc++] = str[i++];
+		vidptr[current_loc++] = 0x07;
+	}
+}
+
+void println(void)
+{
+	unsigned int line_size = BYTES_FOR_EACH_ELEMENT * COLUMNS_IN_LINE;
+	current_loc = current_loc + (line_size - current_loc % (line_size));
+}
+
+void clearscreen(void)
+{
+	unsigned int i = 0;
+	while (i < SCREENSIZE) {
+		vidptr[i++] = ' ';
+		vidptr[i++] = 0x07;
+	}
+}
