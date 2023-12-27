1aa2568979abec746bb4a96d9d47da9decef2d37
SetScript <101998415+SetScript@users.noreply.github.com>
2023-12-27 03:56:53 +0300

    add init

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,33 +1,11 @@
 #include "idt.c"
 #include "../rlibc/stdio.h"
 #include "../rlibc/time.h"
+#include "../rlibc/font.h"
+#include "../rinit/rinit.c"
 
 void kmain(void) {
-	const char *logo[7] = {
-		" ______   .______    _______ .__   __.     .______       _______ ___   ___ ",
-		" /  __  %  |   _  %  |   ____||  % |  |    |   _  %     |   ____|%  % /  / ",
-		"|  |  |  | |  |_)  | |  |__   |   %|  |    |  |_)  |    |  |__    %  V  /  ",
-		"|  |  |  | |   ___/  |   __|  |  . `  |    |      /     |   __|    >   <   ",
-		"|  `--'  | |  |      |  |____ |  |%   |    |  |%  %----.|  |____  /  .  %  ",
-		" %______/  | _|      |_______||__| %__|    | _| `._____||_______|/__/ %__% ",
-		"                                           - current version: v0.1 indev"
-	};
-	
-	int i = 0;
-	while(i < 7) {
-		println(logo[i]);
-		i++;
-	}
-	i = 0;
-
-	newline();
-	newline();
-
-	println(":: Kernel running.");
-	sleep(1);
-	println(":: IDT loaded.");
-	sleep(1);
-	println(":: Rlibc loaded.");
-
+	rinit(1);
+	rinit(2);
 	return;
 }
