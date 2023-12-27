b1d006ff5840f5be4fb1e3f8ee2b5671c34480c1
SetScript <101998415+SetScript@users.noreply.github.com>
2023-12-27 23:05:59 +0300

    remove font

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,11 +1,14 @@
 #include "idt.c"
 #include "../rlibc/stdio.h"
 #include "../rlibc/time.h"
-#include "../rlibc/font.h"
 #include "../rinit/rinit.c"
 
 void kmain(void) {
 	rinit(1);
 	rinit(2);
+
+	println("###",15);
+	println("###",1);
+	println("###",4);
 	return;
 }
