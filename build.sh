2d4da3e25cd7f99e9a70a4ebc6cc2f93d03b4f26
SetScript <101998415+SetScript@users.noreply.github.com>
2024-01-03 05:01:00 +0300

    add build.sh file

---
+++ build.sh
@@ -0,0 +1,7 @@
+nasm -f elf32 src/kernel/kernel.asm -o kasm.o
+gcc -fno-stack-protector -m32 -c src/kernel/kernel.c -o kc.o
+ld -m elf_i386 -T link.ld -o kernel kasm.o kc.o
+mv kernel build/boot/kernel.bin
+grub-mkrescue -o build.iso build
+rm kasm.o kc.o
+qemu-system-i386 -m 50m -cdrom build.iso
