0edb54efc2004d2500be568ac204f64bf542e1e7
SetScript <101998415+SetScript@users.noreply.github.com>
2024-01-03 19:23:51 +0300

    update build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ ld -m elf_i386 -T link.ld -o kernel kasm.o kc.o
 mv kernel build/boot/kernel.bin
 grub-mkrescue -o build.iso build
 rm kasm.o kc.o
-qemu-system-i386 -m 50M -cdrom build.iso
+qemu-system-i386 -m 40M -cdrom build.iso
